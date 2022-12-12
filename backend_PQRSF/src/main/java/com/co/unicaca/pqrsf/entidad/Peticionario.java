1a7e538839ad7a4112037da68adf0aec2e1c7fdd
Edwin Adrada Ruiz <jesusadrada@unicauca.edu.co>
2022-12-12 15:19:53 -0500

    Correcion de bug que se presentaba al agregar un peticionario desde la PQRSF

--- backend_PQRSF/src/main/java/com/co/unicaca/pqrsf/entidad/Peticionario.java
+++ backend_PQRSF/src/main/java/com/co/unicaca/pqrsf/entidad/Peticionario.java
@@ -1,10 +1,15 @@
 package com.co.unicaca.pqrsf.entidad;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 
